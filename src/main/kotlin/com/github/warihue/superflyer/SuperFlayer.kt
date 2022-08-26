87920ac32e36af19b52defbac7e73c810c30e40b
WariHue <warihue2428@gmail.com>
2022-08-26 12:49:41 +0900

    new thing2

--- src/main/kotlin/com/github/warihue/superflyer/SuperFlayer.kt
+++ src/main/kotlin/com/github/warihue/superflyer/SuperFlayer.kt
@@ -1,11 +1,28 @@
 package com.github.warihue.superflyer
 
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.event.EventHandler
+import org.bukkit.entity.Damageable
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerJoinEvent
 
 
-class SuperFlayer : JavaPlugin(){
+abstract class SuperFlayer : JavaPlugin(){
 
+    var pBoard = HashMap<String, Player>()
     override fun onEnable() {
         println('잉')
     }
+    fun setPlayer(p : Player, pM : String){
+        pBoard.put(pM, p)
+        p.damage(10.00, p)
+    }
+    @EventHandler
+    fun interect(pla : PlayerJoinEvent){
+        var p : Player
+        p = pla.player
+        setPlayer(p, p.name)
+    }
+
 }
